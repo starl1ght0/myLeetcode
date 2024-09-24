56d17b27df4c03a1d59d7a80198fd85901316ef4
Ellerywi || Starl1ght || Ternovoy <st4rl1ght0s@gmail.com>
2024-09-24 22:32:59 +0700

    Create binary_search.py

---
+++ Python/easy/binary_search.py
@@ -0,0 +1,18 @@
+# task №704
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) // 2
+            guess = nums[mid]
+            if guess == target:
+                return mid
+            if guess > target:
+                high = mid - 1
+            else:
+                low = mid + 1
+        
+        return -1
