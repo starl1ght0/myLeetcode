6b8e23b00e02fe788bf2851abd1a38bd6224d910
Ellerywi || Starl1ght || Ternovoy <st4rl1ght0s@gmail.com>
2024-09-27 16:58:50 +0700

    Update Search_Insert_Position.cpp

--- c++/easy/Search_Insert_Position.cpp
+++ c++/easy/Search_Insert_Position.cpp
@@ -1,1 +1,27 @@
 // task №35. 
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size();
+        int mid = 0;
+        if(target>nums[r-1]){
+            return {r};
+        }
+
+        while (l <= r) {
+            mid = (r + l) / 2;
+            int guess = nums[mid];
+
+            if (guess == target) {
+                return mid;
+            } else if (guess > target) {
+                r = mid - 1;
+            } else if (guess < target) {
+                l = mid + 1;
+            } 
+        }
+        return l;
+    }
+};
