9a9acda9dafc0968fd24ec472589283a9cb63be1
Ellerywi || Starl1ght || Ternovoy <st4rl1ght0s@gmail.com>
2024-10-13 01:09:06 +0700

    Update and rename Create_a_New_Column.ipynb to Create_a_New_Column.py

--- Pandas/easy/Create_a_New_Column.py
+++ Pandas/easy/Create_a_New_Column.py
@@ -1,5 +1,7 @@
 import pandas as pd
 
+# task №2881 
+
 def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
     employees = employees.assign(bonus = employees['salary'] * 2)
     return employees
