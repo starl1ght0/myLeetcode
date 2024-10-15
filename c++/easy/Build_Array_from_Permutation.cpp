661b77b7a6b4ae20719946061949fcd6d2355db4
Ellerywi || Starl1ght || Ternovoy <st4rl1ght0s@gmail.com>
2024-10-15 23:01:36 +0700

    Update Build_Array_from_Permutation.cpp

--- c++/easy/Build_Array_from_Permutation.cpp
+++ c++/easy/Build_Array_from_Permutation.cpp
@@ -3,10 +3,10 @@
 class Solution {
 public:
     vector<int> buildArray(vector<int>& nums) {
-        vector<int> ans;
+        vector<int> ans(nums.size(), 0);
 
         for (int i = 0; i < nums.size(); i++) {
-            ans.push_back(nums[nums[i]]);
+            ans[i] = nums[nums[i]];
         }
     return ans;
     }
