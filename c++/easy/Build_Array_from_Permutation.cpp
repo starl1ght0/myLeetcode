083f401a79a2b77f3098a14407ff4909dcad037c
Ellerywi || Starl1ght || Ternovoy <st4rl1ght0s@gmail.com>
2024-10-15 22:56:25 +0700

    Create Build_Array_from_Permutation.cpp

---
+++ c++/easy/Build_Array_from_Permutation.cpp
@@ -0,0 +1,13 @@
+// task №1920
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+            ans.push_back(nums[nums[i]]);
+        }
+    return ans;
+    }
+};
