341fe3a4ad4add1889a8983ea5ca9a5ba9bbb0a5
Ellerywi || Starl1ght || Ternovoy <st4rl1ght0s@gmail.com>
2024-10-16 21:18:35 +0700

    Update Modify_Columns.py

--- Pandas/easy/Modify_Columns.py
+++ Pandas/easy/Modify_Columns.py
@@ -5,3 +5,8 @@ import pandas as pd
 def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
     employees['salary'] = employees['salary'].apply(lambda x: x * 2)
     return employees
+
+'''def modifySalaryColumn(employees: pd.DataFrame) -> pd.DataFrame:
+employees['salary'] = employees['salary'] * 2
+return employees 
+'''
